9d18db0d89b59ee6e5de8de8160a5ff027a149fa
Warsame Ahmed <wahmed04@dcs.bbk.ac.uk>
2020-04-07 16:35:04 +0100

    initial commit

---
+++ src/com/qac/day1/Runner.java
@@ -0,0 +1,18 @@
+package com.qac.day1;
+
+public class Runner {
+	public static void main(String args[]){
+		int age = 25;
+		int height = 6;
+		String homeAdd = "123 example road";
+		Boolean bool = true;
+		
+		System.out.println("Hello World");
+		System.out.println("I am " + age);
+		System.out.println("I am " + height + "ft tall");
+		System.out.println(homeAdd);
+		System.out.println("The value of bool is " + bool);
+		System.out.println("My height is "+height+"ft tall and my address is "+homeAdd);
+	}
+
+}
